5de98dd9c70d0bd60c5e663d5be58c1732b792cc
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-13 15:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 function threeSum(arr, target) {
-// write your code here
-	arr.sort((a, b) => a - b);
-	let diff = Infinity;
-	let ansSum = Infinity;
+  arr.sort((a, b) => a - b); // Sort the array in ascending order
 
-	for(let i=0;i<arr.length-2;i++){
-		let left = i+1;
-		let right = arr.length-1;
+  let closestSum = Infinity; // Variable to track the closest sum
+  let closestDiff = Infinity; // Variable to track the difference between the closest sum and the target
 
-		while(left < right){
-			let sum = arr[i] + arr[left] + arr[right];
-			let currDiff = Math.abs(target - sum);
-			
-			if(currDiff < diff){
-				diff = currDiff;
-				ansSum =  sum;
-			}
-			
-			if(sum === target){
-				return sum;
-			}
-			else if(sum < target){
-				left++;
-			}
-			else{
-				right++;
-			}
-		}
-	}
-	return ansSum;
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right =arr.length - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      const diff = Math.abs(sum - target);
+
+      if (diff < closestDiff) {
+        closestDiff = diff;
+        closestSum = sum;
+      }
+
+      if (sum === target) {
+        return sum; // Found an exact match, return the sum immediately
+      } else if (sum < target) {
+        left++; // Increment left pointer if the sum is smaller than the target
+      } else {
+        right--; // Decrement right pointer if the sum is larger than the target
+      }
+    }
+  }
+
+  return closestSum;
 }
 
 module.exports = threeSum;
