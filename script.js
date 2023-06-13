80e6da9f27b3f4e82ed6c55e803403be199f8b5e
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-13 15:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function threeSum(arr, target) {
 // write your code here
-	nums.sort((a, b) => a - b);
+	arr.sort((a, b) => a - b);
 	let diff = Infinity;
 	let ansSum = Infinity;
 
@@ -11,22 +11,21 @@ function threeSum(arr, target) {
 		while(left < right){
 			let sum = arr[i] + arr[left] + arr[right];
 			let currDiff = Math.abs(target - sum);
-
-			if(sum === target){
-				return sum;
-			}
+			
 			if(currDiff < diff){
 				diff = currDiff;
 				ansSum =  sum;
 			}
-
-			if(sum < target){
+			
+			if(sum === target){
+				return sum;
+			}
+			else if(sum < target){
 				left++;
 			}
 			else{
 				right++;
 			}
-			
 		}
 	}
 	return ansSum;
