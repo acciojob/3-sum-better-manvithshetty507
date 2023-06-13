75bc65c2ae8534890c5597f80e86efb37c854f7e
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-13 12:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
 // write your code here
+	let ans = [];
+	diff = 100000;
+  for(let i=0;i<arr.length-2;i++){
+	  let x = arr[i];
+	  for(let j=i+1;j<arr.length-1;j++){
+		  let y = arr[j];
+		  for(let k=j+1;k<arr.length;k++){
+			  let z = arr[k];
+			  if(target - (x + y + z) < diff){
+				  diff = target - (x + y + z);
+				  ans[0] = x ; ans[1] = y; ans[2] = z;
+			  }
+		  }
+	  }
+  }
+	return ans;
   
 }
 
